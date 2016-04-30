6b950833c048034ddc4d1be18935a8d5615606cb
Abhishek Sen <abhishekpsen@gmail.com>
2016-04-30 10:28:10 -0400

    Added Thomas' sql

--- BigQueryScripts.sql
+++ BigQueryScripts.sql
@@ -25,3 +25,15 @@ group by platform_id
 SELECT platform_id, count(1) as updates FROM [google_cloud_data.machine_events]
 where event_type = 2
 group by platform_id
+
+
+--Jobs scheduled and then failed, killed or lost
+select job_id, count(job_id) from [google_cloud_data.task_events] where job_id in
+(select job_id from [google_cloud_data.task_events] where event_type = 1) AND event_type in (3,5,6) group by job_id limit 100
+
+-- Jobs submitted and then evicted
+select job_id, count(job_id) from [google_cloud_data.task_events] where job_id in
+(select job_id from [google_cloud_data.task_events] where event_type = 0) AND event_type = 4 group by job_id limit 100
+
+-- Canonical memory and Requested memory
+select timestamp, canonical_mem, requested_memory from [google_cloud_data.task_events] JOIN [google_cloud_data.task_usage] ON [google_cloud_data.task_events.job_id] = [google_cloud_data.task_usage.job_id] limit 100
